d9a6749fa67ddf9be6990c6bc5084fd9b3df656f
Brenda Rearden <brenda@rancher.com>
2020-07-21 10:46:13 -0700

    Change verify-install

--- scripts/create_istio_system.sh
+++ scripts/create_istio_system.sh
@@ -4,7 +4,7 @@ old_versions=$(kubectl get --ignore-not-found=true deploy istio-pilot -n istio-s
 # check if older version of istio is still installed in istio-system namespace
 if [[ $old_versions == *"1.3"* || $old_versions == *"1.4"* || $old_versions == *"1.5"* ]]; then
     echo "Please uninstall current istio version before attempting to install $ISTIO_VERSION"
-fi 
+fi
 
 versions=$(kubectl get --ignore-not-found=true deploy istiod -n istio-system -o=jsonpath='{$.spec.template.spec.containers[*].image}')
 if [[ $versions == "" || $versions == *"$ISTIO_VERSION"* ]]
@@ -13,21 +13,25 @@ then
     then
         echo "Running istioctl install with overlay"
         istioctl install -y  -f /app/istio-base.yaml -f /app/overlay-config.yaml
-        istioctl verify-install -f /app/istio-base.yaml -f /app/overlay-config.yaml
-    else 
+        istioctl manifest generate -f /app/istio-base.yaml -f /app/overlay-config.yaml > /app/generated-manifest.yaml
+        istioctl verify-install -f /app/generated-manifest.yaml
+    else
         echo "Running istioctl install"
         istioctl install -y -f /app/istio-base.yaml
-        istioctl verify-install -f /app/istio-base.yaml
+        istioctl manifest generate -f /app/istio-base.yaml > /app/generated-manifest.yaml
+        istioctl verify-install -f /app/generated-manifest.yaml
     fi
 else
     if test -f "/app/overlay-config.yaml"
     then
         echo "Running istioctl upgrade with overlay"
         istioctl upgrade -y  -f /app/istio-base.yaml -f /app/overlay-config.yaml
-        istioctl verify-install -f /app/istio-base.yaml -f /app/overlay-config.yaml
-    else 
+        istioctl manifest generate -f /app/istio-base.yaml -f /app/overlay-config.yaml > /app/generated-manifest.yaml
+        istioctl verify-install -f /app/generated-manifest.yaml
+    else
         echo "Running istioctl upgrade"
         istioctl upgrade -y -f /app/istio-base.yaml
-        istioctl verify-install -f /app/istio-base.yaml
-    fi 
-fi 
+        istioctl manifest generate -f /app/istio-base.yaml > /app/generated-manifest.yaml
+        istioctl verify-install -f /app/generated-manifest.yaml
+    fi
+fi
