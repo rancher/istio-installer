64d3e4edac6d452f36a704331951a06e955dc8e7
Brenda Rearden <brenda@rancher.com>
2020-07-23 13:32:53 -0700

    Changing to always upgrade after intial install

--- scripts/create_istio_system.sh
+++ scripts/create_istio_system.sh
@@ -7,7 +7,7 @@ if [[ $old_versions == *"1.3"* || $old_versions == *"1.4"* || $old_versions == *
 fi
 
 versions=$(kubectl get --ignore-not-found=true deploy istiod -n istio-system -o=jsonpath='{$.spec.template.spec.containers[*].image}')
-if [[ $versions == "" || $versions == *"$ISTIO_VERSION"* ]]
+if [[ $versions == "" ]]
 then
     if test -f "/app/overlay-config.yaml"
     then
--- scripts/uninstall_istio_system.sh
+++ scripts/uninstall_istio_system.sh
@@ -3,10 +3,10 @@ echo "setting up kubeconfig"
 ./usr/local/app/scripts/init_kubeconfig.sh
 if test -f "/app/overlay-config.yaml"
 then
-    echo "Unistalling istio and overlay file"
+    echo "Uninstalling istio and overlay file"
     istioctl manifest generate -f /app/istio-base.yaml -f /app/overlay-config.yaml | kubectl delete -f -
 else
-    echo "Unistalling istio"
+    echo "Uninstalling istio"
     istioctl manifest generate -f /app/istio-base.yaml | kubectl delete -f -
 fi
 kubectl delete namespace istio-system
