95f950f8012483c7bc31e5cea89344c9c91b0fcd
Brenda Rearden <brenda@rancher.com>
2020-07-22 19:10:36 -0700

    Correct uninstall file

--- scripts/uninstall_istio_system.sh
+++ scripts/uninstall_istio_system.sh
@@ -2,10 +2,11 @@
 echo "setting up kubeconfig"
 ./usr/local/app/scripts/init_kubeconfig.sh
 if test -f "/app/overlay-config.yaml"
-then 
-    echo "Unistalling istio and overlay file"    
+then
+    echo "Unistalling istio and overlay file"
     istioctl manifest generate -f /app/istio-base.yaml -f /app/overlay-config.yaml | kubectl delete -f -
-else 
+else
     echo "Unistalling istio"
     istioctl manifest generate -f /app/istio-base.yaml | kubectl delete -f -
+fi
 kubectl delete namespace istio-system
