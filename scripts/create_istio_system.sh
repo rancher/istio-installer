804b1cd72e241fc401150a876fd52bf5175874c2
Brenda Rearden <brenda@rancher.com>
2020-08-11 18:46:21 -0700

    Refactor install, upgrade and delete

--- scripts/create_istio_system.sh
+++ scripts/create_istio_system.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 istioctl version -i $ISTIO_NAMESPACE
-if [[ $FORCE_INSTALL == false ]]; then
+
+ISTIO_FILES=("/app/istio-base.yaml")
+if test -f "/app/overlay-config.yaml"; then
+    echo "creating istio system using overlay"
+    ISTIO_FILES+=("/app/overlay-config.yaml")
+fi
+
+if [[ $FORCE_INSTALL != true ]]; then
   old_versions=$(kubectl get --ignore-not-found=true deploy istio-pilot -n istio-system -o=jsonpath='{$.spec.template.spec.containers[*].image}')
   # check if older version of istio is still installed in istio-system namespace
   if [[ $old_versions == *"1.3"* || $old_versions == *"1.4"* || $old_versions == *"1.5"* ]]; then
-      echo "please uninstall current istio version before attempting to install $ISTIO_VERSION"
+      echo "please uninstall current istio version in istio-system namespace before attempting to install $ISTIO_VERSION"
       exit 1
   fi
   # force install will skip file checks. same analyze command is valid for install and upgrade
-  if test -f "/app/overlay-config.yaml"; then
-    echo "running istioctl analyze with overlay"
-    istioctl analyze /app/istio-base.yaml /app/overlay-config.yaml -i $ISTIO_NAMESPACE --failure-threshold Error
-  else
-    echo "running istioctl analyze"
-    istioctl analyze /app/istio-base.yaml -i $ISTIO_NAMESPACE --failure-threshold Error
-  fi
+  istioctl analyze $ISTIO_FILES -i $ISTIO_NAMESPACE --failure-threshold Error
   if [[ $? -ne 0 ]]; then
     echo "error found during istioctl analyze"
     exit 1
@@ -23,27 +24,12 @@ fi
 
 versions=$(kubectl get --ignore-not-found=true deploy istiod -n $ISTIO_NAMESPACE -o=jsonpath='{$.spec.template.spec.containers[*].image}')
 if [[ $CANARY_REVISION ]] || [[ $versions == "" ]]; then
-    if test -f "/app/overlay-config.yaml"; then
-        echo "running istioctl install with overlay"
-        istioctl install -y  -f /app/istio-base.yaml -f /app/overlay-config.yaml
-        istioctl manifest generate -f /app/istio-base.yaml -f /app/overlay-config.yaml > /app/generated-manifest.yaml
-        istioctl verify-install -i $ISTIO_NAMESPACE -f /app/generated-manifest.yaml
-    else
-        echo "running istioctl install"
-        istioctl install -y -f /app/istio-base.yaml
-        istioctl manifest generate -f /app/istio-base.yaml > /app/generated-manifest.yaml
-        istioctl verify-install -i $ISTIO_NAMESPACE -f /app/generated-manifest.yaml
-    fi
+  echo "running istioctl install"
+  istioctl install -i $ISTIO_NAMESPACE -y ${ISTIO_FILES[@]/#/-f }
+
 else
-    if test -f "/app/overlay-config.yaml"; then
-        echo "running istioctl upgrade with overlay"
-        istioctl upgrade -y  -f /app/istio-base.yaml -f /app/overlay-config.yaml
-        istioctl manifest generate -f /app/istio-base.yaml -f /app/overlay-config.yaml > /app/generated-manifest.yaml
-        istioctl verify-install -i $ISTIO_NAMESPACE -f /app/generated-manifest.yaml
-    else
-        echo "running istioctl upgrade"
-        istioctl upgrade -y -f /app/istio-base.yaml
-        istioctl manifest generate -f /app/istio-base.yaml > /app/generated-manifest.yaml
-        istioctl verify-install -i $ISTIO_NAMESPACE -f /app/generated-manifest.yaml
-    fi
+  echo "running istioctl upgrade"
+  istioctl upgrade -i $ISTIO_NAMESPACE -y ${ISTIO_FILES[@]/#/-f }
 fi
+istioctl manifest generate -i $ISTIO_NAMESPACE ${ISTIO_FILES[@]/#/-f } > /app/generated-manifest.yaml
+istioctl verify-install -i $ISTIO_NAMESPACE -f /app/generated-manifest.yaml
--- scripts/uninstall_istio_system.sh
+++ scripts/uninstall_istio_system.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo "setting up kubeconfig"
 ./usr/local/app/scripts/init_kubeconfig.sh
+
+ISTIO_FILES=("/app/istio-base.yaml")
 if test -f "/app/overlay-config.yaml"; then
-    echo "uninstalling istio and overlay file"
-    istioctl manifest generate -i $ISTIO_NAMESPACE -f /app/istio-base.yaml -f /app/overlay-config.yaml | kubectl delete --ignore-not-found=true -f -
-else
-    echo "uninstalling istio"
-    istioctl manifest generate -i $ISTIO_NAMESPACE -f /app/istio-base.yaml | kubectl delete --ignore-not-found=true -f -
+    echo "uninstalling istio system using overlay"
+    ISTIO_FILES+=("/app/overlay-config.yaml")
 fi
+
+echo "uninstalling istio"
+istioctl manifest generate -i $ISTIO_NAMESPACE ${ISTIO_FILES[@]/#/-f } | kubectl delete --ignore-not-found=true -f -
