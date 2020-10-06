ac847ac0c4238abda423bb61204d2fec4ceb9de7
Bastian Hofmann <bashofmann@gmail.com>
2020-10-06 17:52:09 +0200

    Add Grafana dashboards to rancher-monitoring if installed
    This adds the istio Grafana dashboards for the correct istio version to the correct rancher-monitoring dashboards namespace, if rancher-monitoring is installed.
    Dashboards are downloaded at build time so that the installation is not relying on grafana.com to be available or reachable (e.g. in air-gapped installations).
    
    Signed-off-by: Bastian Hofmann <bashofmann@gmail.com>

--- scripts/uninstall_istio_system.sh
+++ scripts/uninstall_istio_system.sh
@@ -10,3 +10,7 @@ fi
 
 echo "uninstalling istio"
 istioctl manifest generate -i $ISTIO_NAMESPACE ${ISTIO_FILES[@]/#/-f } | kubectl delete --ignore-not-found=true -f -
+
+if kubectl get namespace cattle-dashboards; then
+  kubectl delete configmap -n cattle-dashboards -l istio_dashboard=1
+fi
