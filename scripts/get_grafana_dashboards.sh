d20d5aa3463e84a7f480642aca43475c483d6d04
Brenda Rearden <brenda@rancher.com>
2021-06-07 15:21:22 -0700

    Ensure jq filter returns 1 result

--- scripts/get_grafana_dashboards.sh
+++ scripts/get_grafana_dashboards.sh
@@ -2,6 +2,6 @@
 
 # https://istio.io/latest/docs/ops/integrations/grafana/#option-1-quick-start
 for DASHBOARD in 7639 11829 7636 7630 7642 7645; do
-  REVISION="$(curl -s https://grafana.com/api/dashboards/${DASHBOARD}/revisions -s | jq ".items[] | select(.description | contains(\"${ISTIO_VERSION}\")) | .revision")"
+  REVISION="$(curl -s https://grafana.com/api/dashboards/${DASHBOARD}/revisions -s | jq ".items[] | select(.description | endswith(\"${ISTIO_VERSION}\")) | .revision")"
   curl -s https://grafana.com/api/dashboards/${DASHBOARD}/revisions/${REVISION}/download > /usr/local/app/dashboards/${DASHBOARD}.json
 done
