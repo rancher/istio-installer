7d7e9d002525a862d448a95c67cc5847040c1a59
Alex Seymour <axeal@users.noreply.github.com>
2022-01-31 09:50:21 +0100

    Add check that dashboard revision exists

--- scripts/get_grafana_dashboards.sh
+++ scripts/get_grafana_dashboards.sh
@@ -3,5 +3,7 @@
 # https://istio.io/latest/docs/ops/integrations/grafana/#option-1-quick-start
 for DASHBOARD in 7639 11829 7636 7630 7642 7645; do
   REVISION="$(curl -s https://grafana.com/api/dashboards/${DASHBOARD}/revisions -s | jq ".items[] | select(.description | endswith(\"${ISTIO_VERSION}\")) | .revision")"
-  curl -s https://grafana.com/api/dashboards/${DASHBOARD}/revisions/${REVISION}/download > /usr/local/app/dashboards/${DASHBOARD}.json
+  if [[ $REVISION =~ ^[0-9]+$ ]]; then
+    curl -s https://grafana.com/api/dashboards/${DASHBOARD}/revisions/${REVISION}/download > /usr/local/app/dashboards/${DASHBOARD}.json
+  fi
 done
