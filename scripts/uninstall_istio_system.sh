b95273f7712b978892de14ac80b78562071ef4a4
Vaishnav Gaikwad <vaishnavgaikwad721@gmail.com>
2022-04-19 11:37:46 +0530

    cleanup mutatingwebhookconfiguration
    Remove the mutatingwebhookconfiguration which is left behind after uninstalling istio.

--- scripts/uninstall_istio_system.sh
+++ scripts/uninstall_istio_system.sh
@@ -12,6 +12,10 @@ fi
 echo "uninstalling istio"
 istioctl manifest generate -i $ISTIO_NAMESPACE ${ISTIO_FILES[@]/#/-f } | kubectl delete --ignore-not-found=true -f -
 
+if kubectl get mutatingwebhookconfiguration istio-revision-tag-default -n istio-system; then
+  kubectl delete mutatingwebhookconfiguration  istio-revision-tag-default -n istio-system
+fi
+
 if kubectl get namespace cattle-dashboards; then
   kubectl delete configmap -n cattle-dashboards -l istio_dashboard=1
 fi
